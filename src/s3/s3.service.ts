5f0efeab18a8a15ed9a2d763ca35ed51b7cfaa69
Kushagra <kushagra.gupta@appinventiv.com>
2025-04-25 14:16:46 +0530

    commit

--- src/s3/s3.service.ts
+++ src/s3/s3.service.ts
@@ -4,9 +4,26 @@ import { randomUUID } from 'crypto';
 
 @Injectable()
 export class S3Service {
- 
+  private s3 = new S3Client({
+    region: process.env.AWS_REGION,
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    },
+  });
 
   async uploadFile(file: Express.Multer.File): Promise<string> {
-    
+    const key = `profiles/${randomUUID()}-${file.originalname}`;
+
+    const command = new PutObjectCommand({
+      Bucket: process.env.AWS_S3_BUCKET_NAME,
+      Key: key,
+      Body: file.buffer,
+      ContentType: file.mimetype,
+    });
+
+    await this.s3.send(command);
+
+    return `https://${process.env.AWS_S3_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${key}`;
   }
 }
