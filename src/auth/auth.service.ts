2ae52230d6844440b6ed4068aa4099b858979473
Kushagra <kushagra.gupta@appinventiv.com>
2025-04-30 11:13:39 +0530

    fixed bug

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -14,92 +14,107 @@ export class AuthService {
 
   private transporter: nodemailer.Transporter;
 
-    constructor(
-        private readonly userService: UserService,
-        private jwtService: JwtService,
-        @Inject('REDIS_CLIENT') private readonly redis: Redis,
-        private readonly configService: ConfigService,
-        // @InjectModel(User) private userModel: typeof User
-    ) { 
-      this.transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-          user: this.configService.get<string>('EMAIL_USER'),
-          pass: this.configService.get<string>('EMAIL_PASS'),
-        },
-      });
+  constructor(
+    private readonly userService: UserService,
+    private jwtService: JwtService,
+    @Inject('REDIS_CLIENT') private readonly redis: Redis,
+    private readonly configService: ConfigService,
+    // @InjectModel(User) private userModel: typeof User
+  ) {
+    this.transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: this.configService.get<string>('EMAIL_USER'),
+        pass: this.configService.get<string>('EMAIL_PASS'),
+      },
+    });
+  }
+
+
+
+
+  async signup(signupDto: SignupDto) {
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(signupDto.password, saltRounds);
+    await this.userService.create({
+      ...signupDto,
+      password: hashedPassword,
+    });
+    return { message: 'User created successfully' };
+  }
+
+  async login(loginDto: LoginDto) {
+    const user = await this.userService.findByEmail(loginDto.email);
+    console.log(user);
+    if (!user) {
+      return { message: "User does not exist" };
+    }
+    const isPasswordCorrect = await bcrypt.compare(loginDto.password, user.dataValues.password);
+
+    if (!isPasswordCorrect) {
+      return { message: "Wrong credentials" };
     }
 
-    
-    
+    const payload = { sub: user.id };
+    const token = this.jwtService.sign(payload);
+    return { message: "Login successful", Token: token };
+  }
 
-    async signup(signupDto: SignupDto) {
-        const saltRounds = 10;
-        const hashedPassword = await bcrypt.hash(signupDto.password, saltRounds);
-        await this.userService.create({
-            ...signupDto,
-            password: hashedPassword,
-        });
-        return { message: 'User created successfully'};
+
+  async forgetPassword(forgetPasswordDto: ForgetPasswordDto) {
+    const verifiedKey = `otp_verified:${forgetPasswordDto.email}`;
+    const isVerified = await this.redis.get(verifiedKey);
+
+    if (!isVerified) {
+      throw new UnauthorizedException('OTP not verified or session expired');
+    }
+    const result = await this.userService.changePassword(forgetPasswordDto);
+
+    await this.redis.del(verifiedKey);
+
+    return result;
+  }
+
+  async sendOtp(email: string) {
+    console.log(email);
+    const otp = Math.floor(100000 + Math.random() * 900000).toString();
+    const key = `otp:${email}`;
+
+    await this.redis.set(key, otp, 'EX', 300);
+
+    const mailOptions = {
+      from: '"kushagra.gupta@appinventiv.com>',
+      to: email,
+      subject: 'Your OTP Code',
+      text: `Your OTP code is: ${otp}`,
+      html: `<b>Your OTP code is:</b> <h2>${otp}</h2>`,
+    };
+
+    // Send email
+    await this.transporter.sendMail(mailOptions);
+
+    return { message: 'OTP sent successfully' };
+  }
+
+  async verifyOtp(email: string, submittedOtp: string) {
+    const key = `otp:${email}`;
+    const storedOtp = await this.redis.get(key);
+
+    if (!storedOtp) {
+      throw new UnauthorizedException('OTP expired or not found');
+    }
+    if (storedOtp !== submittedOtp) {
+      throw new UnauthorizedException('Invalid OTP');
     }
 
-    async login(loginDto: LoginDto) {
-        const user= await this.userService.findByEmail(loginDto.email);
-        console.log(user);
-        if(!user){
-            return {message: "User does not exist"};
-        }
-        const isPasswordCorrect = await bcrypt.compare(loginDto.password, user.dataValues.password);
+    await this.redis.del(key);
 
-        if(!isPasswordCorrect){
-            return {message: "Wrong credentials"};
-        }
+    const verifiedKey = `otp_verified:${email}`;
+    await this.redis.set(verifiedKey, 'true', 'EX', 600);
 
-        const payload = {sub: user.id};
-        const token = this.jwtService.sign(payload);
-        return {message: "Login successful", Token: token};
-    }
+    return { message: 'OTP verified successfully' };
+  }
 
 
-    async forgetPassword(forgetPasswordDto: ForgetPasswordDto){
-        return await this.userService.changePassword(forgetPasswordDto);
-    }
 
-    async sendOtp(email: string) {
-        console.log(email);
-        const otp = Math.floor(100000 + Math.random() * 900000).toString();
-        const key = `otp:${email}`;
-      
-        await this.redis.set(key, otp, 'EX', 300);
-      
-        const mailOptions = {
-          from: '"kushagra.gupta@appinventiv.com>',
-          to: email,
-          subject: 'Your OTP Code',
-          text: `Your OTP code is: ${otp}`,
-          html: `<b>Your OTP code is:</b> <h2>${otp}</h2>`,
-        };
-      
-        // Send email
-        await this.transporter.sendMail(mailOptions);
-      
-        return { message: 'OTP sent successfully' };
-      }
-
-      async verifyOtp(email: string, submittedOtp: string) {
-        const key = `otp:${email}`;
-        const storedOtp = await this.redis.get(key);
-      
-        if (!storedOtp) {
-          throw new UnauthorizedException('OTP expired or not found');
-        }
-        if (storedOtp !== submittedOtp) {
-          throw new UnauthorizedException('Invalid OTP');
-        }
-        await this.redis.del(key);
-      
-        return { message: 'OTP verified successfully' };
-      }
-      
-      
 }
